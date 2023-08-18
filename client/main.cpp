8c0e09f66427a4ca2819f44ee4f8fa3b9d2a6dfc
Kate March <49290079+PetitBoo@users.noreply.github.com>
2023-08-18 19:42:51 +0000

    Add files via upload

--- client/main.cpp
+++ client/main.cpp
@@ -1,7 +1,8 @@
 #include <unordered_set>
-#include "Client.h"
+#include "client.h"
 #ifdef _WIN64
 #include <conio.h>
+#elif __linux__
 #endif
 
 
@@ -13,40 +14,61 @@ int main() {
 
     connect_to_server(client_socket, "127.0.0.1", 7777);
 
-    
-    std::unordered_set<std::string> messages_not_requiring_response = { "enter 1 - 2 or q\n", "enter 1 - 5 or q\n", "error: user login is busy\n", "error: user name is busy\n" };
-
-    while (true) {
+    bool connect = true;
+    while (connect) {
         int data_size = get_data_size(client_socket);
         if (data_size > 0) {
-            auto data = receive_data(client_socket, data_size);
-            std::string chek(data.get(), data_size);
-            std::cout << chek << std::endl;
-            if (chek == "/End Work")
+            auto data = receive_package(client_socket, data_size);
+            switch (data->flag) {
+            case '1': {
+                std::cout << data->message << std::endl;
+                Package message{ "0", '0' };
+                std::cin >> message.flag;
+                send_package(client_socket, message);
                 break;
-            if (chek == "Enter your message") {
-                char ch;
-                std::string message;
-                while (std::cin.get(ch) && ch != '\n');
-                
-                std::getline(std::cin, message);
-                send_string(client_socket, message);
-                continue;
             }
+            case '2': {
+                std::cout << data->message << std::endl;
+                break;
+            }
+            case '3': {
+                std::cout << data->message << std::endl;
+                Package message{ "0", '0' };
+                std::cin >> message.message;
+                send_package(client_socket, message);
+                break;
+            }
+            case '4': {
+                std::cout << data->message << std::endl;
 
-            
-            if (messages_not_requiring_response.find(chek) == messages_not_requiring_response.end() || chek[0] != '\n') {
-                std::string message;
-                std::cin >> message;
-                send_string(client_socket, message);
+                char ch;
+                Package message{ "0", '0' };
+                while (std::cin.get(ch) && ch != '\n');
+                std::getline(std::cin, message.message);
+                send_package(client_socket, message);
+                continue;
+
+            }
+            case '5': {
+                std::cout << data->message << std::endl;
+                connect = false;
+                break;
+            }
+            default: {
+                std::cout << "error flag" << std::endl;
+            }
             }
         }
     }
+
+
+
 
     close_socket(client_socket);
 
 #ifdef _WIN64
     _getch();
+#elif __linux__
 #endif
 
     return 0;
